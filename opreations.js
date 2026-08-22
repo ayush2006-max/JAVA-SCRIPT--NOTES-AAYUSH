deed5be0b9d9124c5b0e51eaebeb64ed77b79ed0
ayush2006-max <ayushtiwari12@gmail.com>
2026-08-22 15:37:13 +0530

    d

--- opreations.js
+++ opreations.js
@@ -7,14 +7,34 @@
 // non-primitive data type  object, array, function
 
 // array  , data type of array is object, array is a collection of data
-let arr = [1, 2, 3, 4, 5];
+/*let arr = [1, 2, 3, 4, 5];
 // object {} iske andar ka mall object hai
 let obj = { 
     name: "Rahul",  
     age: 25 ,
  }
  // function
- console.log(typeof arr);
+ console.log(typeof arr);   */
+
+ // MEMORY
+ // 2 types = stack(primitive data type) and heap(non-primitive data type)
+ // simple si  baatt   stack ke andr jb hm  copy me chnage krte orgnal can't change
+ // heap ke andr jb hm copy me chnage krte orgnal bhi change ho jata h  
+
+ // eg of stack 
+
+ let name = "Rahul";
+ let anothername = name;
+ anothername = "Rohit";
+ console.log(anothername); // output  rohit
+ console.log(name); // output  rahul cus static me oringal value change nahi hoti   
+
+// eg of heap
+let obj1 = { name: "Rahul", age: 25 };
+let obj2 = obj1;
+obj2.name = "Rohit";
+console.log(obj2.name); // output  rohit
+console.log(obj1.name); // output  rohit cus in heap original value also changes
 
 
 
