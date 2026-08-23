75429858c11ca8c9a7988d0146774358dc9aae20
ayush2006-max <ayushtiwari12@gmail.com>
2026-08-24 00:17:28 +0530

    d

---
+++ Numbers-Maths.js
@@ -0,0 +1,38 @@
+// nums
+const num = 428;
+console.log(num.toString());
+let ac = new Number(42); // you wan make sure its a number datatype
+console.log(ac.toFixed(1)) ;  
+
+
+
+
+// maths
+console.log(Math.abs(-24)); // positive value   
+console.log(Math.round(4.6)); // round to nearest integer 
+console.log(Math.ceil (4.6)); // round to nearest top integer 
+console.log(Math.floor (4.6)); // round to nearest bottom integer
+console.log(Math.min (4,6,7)); // find the minimum value
+console.log(Math.max (4,6,7)); // find the maximum value4
+// random number
+console.log(Math.random()); // random number between 0 and 1
+console.log((Math.random()*10)+1); // random number between 1 and 10
+console.log(Math.floor(Math.random()*10)+1); // random number between 1 and 10 integer
+
+// if you wannn random num btw fix range
+let min = 34;
+const max = 45;
+
+let random = Math.floor(Math.random()*(max-min)+min);
+console.log(random); // random number between 34 and 100
+
+
+
+
+
+
+
+
+
+
+ 
